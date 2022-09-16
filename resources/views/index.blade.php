2cad04bda3e923c08afa512a18159b4499444fcb
Zxyda <oktaviantrisabana@gmail.com>
2022-09-16 12:19:52 +0900

    fix

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,60 @@
 @extends('navbar.layout')
 
 @section('content')
+<head>
+    <style type="text/css">
+        body{
+            background-color: #eee;
+        }
+        .homehead{
+            background: #C0C0C0;
+            padding: 0px 0px 20px 0px;
+        }
+    </style>
+</head>
+
+    <header class="homehead">
+        <div class="container main-menu">
+          <div class="row align-items-center justify-content-between d-flex">
+            <div id="logo">
+              <a href="/" style="color:white; font-size:24px;"><img src="img/ball.png" width="50px" height="50px" alt="" title="Corner"/>Corner</a>
+            </div>
+            <nav id="nav-menu-container">
+              <ul class="nav-menu">
+                <li class="menu-active"><a href="/">Beranda</a></li>
+              @auth
+
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    ようこそ、{{ auth()->user()->name }} さん
+                  </a>
+                  <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <li><a class="dropdown-item" href="{{ url('coupon') }}">my クーポン <i class="fas fa-ticket-alt"></i></a></li>
+                    <li><a class="dropdown-item" href="#">買い物かご</a></li>
+                    <li><hr class="dropdown-divider"></li>
+
+                    <form action="/logout" method="post">
+                      @csrf
+                      <button type="submit" class="dropdown-item text-center">ログアウト <i class="fa fa-sign-out-alt"></i></button>
+                    </form>
+                  
+                  </ul>
+                </li>
+
+                @else
+                <li><a href="{{ URL('register') }}">Registrasi</a></li>
+                <li><a href="{{ URL('mitreg') }}">Daftar Mitra</a></li>
+                <li><a href="{{ route('login') }}">Login <i class="fa fa-sign-in-alt"></i></a></li>
+                </li>
+            @endauth
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </header>
+
+<body>
+    
 	
 	<section class="product-area section-gap">
         <div class="container">
@@ -32,5 +86,6 @@
         </div>
     </section>
 
+</body>
 
 @endsection
